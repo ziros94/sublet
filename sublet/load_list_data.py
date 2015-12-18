f759eac3806a7979a9e7c3ab6ab49a19a23c0d56
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-17 23:36:37 -0500

    better formatted

--- sublet/load_list_data.py
+++ sublet/load_list_data.py
@@ -2,22 +2,22 @@ import django
 import csv
 from app.models import ListingOwned
 from app.models import ApartmentOwned
-#ListingOwned
-django.setup()
 
+django.setup()
 
 with open('list_data.csv','r') as f:
     reader = csv.reader(f)
     for i in range(10000):
-    	apartment = ApartmentOwned.objects.get(pk=(i+20002))
-    	
+    	apartment = ApartmentOwned.objects.get(pk=(i+20002))   	
     	for row in reader:
     		apartment.listings_owned.create(
-       			title = row[0], price = row[1], is_active = row[2],user_pk = row[3],duration = row[4],is_booked = row[5]
+       			title = row[0], 
+       			price = row[1], 
+       			is_active = row[2],
+       			user_pk = row[3],
+       			duration = row[4],
+       			is_booked = row[5]
        	)
-      
 
-#import csv data to ApartmentOwned under user_pk =11
-#to run this, might need to pass the environmental path by pasting the following to the terminal
+#./manage.py dumpdata app.ListingOwned --indent 2 > list_test.json      
 
-#./manage.py dumpdata app.ApartmentOwned --indent 2 > apart_test.json
--- sublet/load_sub_data.py
+++ sublet/load_sub_data.py
@@ -1,18 +1,25 @@
 import django
-django.setup()
 import csv
 from app.models import ApartmentOwned
 from app.models import SubletUser
-#ListingOwned
+
+django.setup()
 
 user = SubletUser.objects.get(pk=2)
 with open('sub_data.csv','r') as f:
     reader = csv.reader(f)
     for row in reader:
     	user.apartments_owned.create(
-       		city = row[0],zip = row[1],sqFt = row[2],has_doorman = row[3],user_pk = row[4],min_from_subway = row[5],state = row[6],street = row[7],year = row[8]
+       		city = row[0],
+       		zip = row[1],
+       		sqFt = row[2],
+       		has_doorman = row[3],
+       		user_pk = row[4],
+       		min_from_subway = row[5],
+       		state = row[6],
+       		street = row[7],
+       		year = row[8]
        	)
-      
 
-#import csv data to ApartmentOwned under user_pk =11
-#to run this, might need to pass the environmental path by pasting the following to the terminal
+#./manage.py dumpdata app.ApartmentOwned --indent 2 > apart_test.json
+      
