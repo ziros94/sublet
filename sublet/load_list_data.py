e3256738af5f1c3d64f0142e3802b4f47bb6e116
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-18 16:38:07 -0500

    fix a bug in load_list_data.py

--- sublet/load_list_data.py
+++ sublet/load_list_data.py
@@ -5,19 +5,28 @@ from app.models import ApartmentOwned
 
 django.setup()
 
-with open('list_data.csv','r') as f:
+def load_list_data():
+  with open('list_data.csv','r') as f:
+    i = 20002
     reader = csv.reader(f)
-    for i in range(10000):
-    	apartment = ApartmentOwned.objects.get(pk=(i+20002))   	
-    	for row in reader:
-    		apartment.listings_owned.create(
-       			title = row[0], 
-       			price = row[1], 
-       			is_active = row[2],
-       			user_pk = row[3],
-       			duration = row[4],
-       			is_booked = row[5]
-       	)
+    for row in reader:
+      #the test apartment data starts with pk_20002 to pk_30001
+      apartment = ApartmentOwned.objects.get(pk=i)  
+      i += 1
+      apartment.listings_owned.create(
+       		title = row[0], 
+       		price = row[1], 
+       		is_active = row[2],
+       		user_pk = row[3],
+       		duration = row[4],
+       		is_booked = row[5])
+def main():
+  load_list_data()
 
+if __name__ == "__main__":
+    main()
+#to view the results in file
 #./manage.py dumpdata app.ListingOwned --indent 2 > list_test.json      
+
+ 
 
