3b50559f908bcdc98153a211400cce3e850ee836
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-17 23:39:27 -0500

    create list_data_generation.py and sub_data_generation.py

--- support/sub_data_generation.py
+++ support/sub_data_generation.py
@@ -2,6 +2,9 @@ import csv
 from random import gauss
 n = 10000
 
+#same city, street, state as these are not factors in the regression model
+#these 10k are under user_pk = 2
+
 def get_sqFT():
     sqFt = []
     while len(sqFt) < n:
@@ -14,7 +17,7 @@ def get_sqFT():
 def get_price():
     price = []
     while len(price) < n:
-        value = gauss(1000, 100)
+        value = gauss(40, 15)
         value = format(value, '.2f')
         price.append(value)
     price.sort(key=lambda p: float(p))
@@ -24,8 +27,9 @@ def get_year():
     year = []
     while len(year) < n:
         value = gauss(1990, 7)
-        value = round(value, 0)
-        year.append(int(value))
+        if value <= 2015:
+            value = round(value, 0)
+            year.append(int(value))
     year.sort(key=lambda p: float(p))
     return year
 
@@ -34,21 +38,21 @@ def get_doorMan():
     #doorMan is binary
     doorMan = []
     while len(doorMan) < frac:
-        value = 1
+        value = 0
         doorMan.append(value)
     while len(doorMan) < n:
-        value = 0
+        value = 1
         doorMan.append(value)
     return doorMan
 
 def get_subwayProximity():
     min_away = []
     while len(min_away) < n:
-        value = gauss(13, 10)
-        if value > 0:
+        value = gauss(20, 10)
+        if value > 2:
             value = round(value, 0)
             min_away.append(int(value))
-    min_away= sorted(min_away)
+    min_away= sorted(min_away, reverse=True)
     return min_away
 
 def get_street_info():
@@ -56,7 +60,6 @@ def get_street_info():
     while len(street) < n:
         value = "14th str."
         street.append(value)
-    #print street
     return street
 
 def get_city_info():
@@ -64,7 +67,6 @@ def get_city_info():
     while len(city) < n:
         value = 'New York'
         city.append(value)
-    #print city
     return city
 
 def get_state_info():
@@ -72,7 +74,6 @@ def get_state_info():
     while len(state) < n:
         value = "NY"
         state.append(value)
-    #print state
     return state
 
 def get_zip():
@@ -80,28 +81,16 @@ def get_zip():
     while len(zip) < n:
         value = "10009"
         zip.append(value)
-    #print zip
     return zip
-
-def get_user_id():
-    user_id = []
-    while len(user_id) < n:
-        value = 11
-        user_id.append(value)
-    #print user_id
-    return user_id
 
 def get_user_pk():
     user_pk = []
     while len(user_pk) < n:
-        value = 11
+        value = 2
         user_pk.append(int(value))
     return user_pk
 
-
-
 def main():
-    price = get_price()
     sqFt = get_sqFT()
     year = get_year()
     doorMan = get_doorMan()
@@ -110,14 +99,10 @@ def main():
     city = get_city_info()
     state = get_state_info()
     zip = get_zip()
-    user_id = get_user_id()
     user_pk = get_user_pk()
     featureset = [[] for x in xrange(n)]
     #featureset[0]= ["city", "zip", "sqFt", "has_doorman","user_pk", "min_from_subway", "state","street", "year"]
     feature_entry = []
-    priceset = [[] for x in xrange(n)]
-    #priceset[0] = ["price"]
-    price_entry = []
 
     for i in range(n):
         feature_entry.append(city[i])
@@ -128,23 +113,13 @@ def main():
         feature_entry.append(min_away[i])
         feature_entry.append(state[i])
         feature_entry.append(street[i])
-        #feature_entry.append(user_id[i])
         feature_entry.append(year[i])
         featureset[i] = feature_entry
         feature_entry = []
-   
-    for i in range(n):
-        price_entry.append(price[i])
-        priceset[i] = price_entry
-        price_entry = []
 
     with open('sub_data.csv', 'w') as fp:
         a = csv.writer(fp, delimiter=',')
         a.writerows(featureset)
-        
-    with open('price_data.csv','w') as fp:
-        b = csv.writer(fp, delimiter=',')
-        b.writerows(priceset)
         
 
 if __name__ == "__main__":
