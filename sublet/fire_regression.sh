91e3c281f5e74b51ede5e06c4aaf93e807db700e
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-19 10:12:26 -0500

    updated fire_regression.sh

--- sublet/fire_regression.sh
+++ sublet/fire_regression.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#This script runs "dump2CSV.py",to export apartment feature and matching price data from db;
+#runs "regression.py" to takes in .csv and outputs regression result to parameters.txt
+#runs "load_parameters.py" to import parameters.txt to Parameters table
+#this will be fired up every 30 minutes by crontab
 echo "cron is working"
 export DJANGO_SETTINGS_MODULE="sublet.settings"
 python /Users/yehua/testing/sublet/sublet/regression.py
