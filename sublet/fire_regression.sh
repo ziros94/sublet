0065b98f08cf4a707f1733fe995f20ccce09e593
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-19 11:21:40 -0500

    updated fire_regression.sh

--- sublet/fire_regression.sh
+++ sublet/fire_regression.sh
@@ -3,6 +3,9 @@
 #runs "regression.py" to take in .csv and outputs regression result to parameters.txt
 #runs "load_parameters.py" to import parameters.txt to Parameters table
 #this will be fired up every 30 minutes by crontab
+#change your path to the files accordingly! 
+#$chmod 755 fire_regression.sh
+
 echo "cron is working"
 export DJANGO_SETTINGS_MODULE="sublet.settings"
 python /Users/yehua/testing/sublet/sublet/regression.py
