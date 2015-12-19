b51742be08d3f8ed9a43da2b7add32c8325d9ac3
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-19 00:10:41 -0500

    add load_parameters.py

---
+++ sublet/load_parameters.py
@@ -0,0 +1,17 @@
+__author__      = "Emily"
+
+import django
+from app.models import Parameters
+
+django.setup()
+
+f = open("parameters.txt", "r")
+def load_parameters(file, city):
+     f = file
+     city = city
+     print f
+     for line in f:
+        x =line.split(' ')
+        parameters = Parameters(city = city, intercept = x[0], sqFt_coef = x[1], year_coef = x[2], has_doorman_coef = x[3], min_from_subway_coef = x[4])
+        parameters.save()
+load_parameters(f, "new york")
