b73b42f3b1095a284d9bf07b08b46e4ac6529707
Ye Hua <yehua@172-16-176-89.DYNAPOOL.NYU.EDU>
2015-12-20 14:06:42 -0500

    remove duplicated hints.py

--- sublet/support/hints.py
+++
@@ -1,7 +0,0 @@
-#tag query_set with user_pk, so routers know where to find it
-def set_user_for_sharding(query_set, user_pk):
-  if query_set._hints == None:
-    query_set._hints = {'user_pk' : user_pk }
-  else:
-    query_set._hints['user_pk'] = user_pk
-
