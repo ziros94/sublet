cbaeb536b19bffbe33cadfba23a53ba5936529ce
Ye Hua <yehua@Yes-MacBook-Pro.home>
2015-12-20 09:39:59 -0500

    remove test_terminal

--- sublet/support/test_terminal.py
+++
@@ -1,5 +0,0 @@
-from app.models import ApartmentOwned
-from app.models import SubletUser
-user = SubletUser.objects.get(pk=11)
-print(dir(ApartmentOwned))
-#test = user.apartments_owned.create(city='New York', zip='10009',sqFt='100',has_doorman='true', user_pk=11,min_from_subway=5, #state='NY',street='14th street',year=1940)
